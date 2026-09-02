ae9fb80a555debc6e17fddb8935f87a311a5c48a
mahabubalom0 <mdmahabubalom611762@gmail.com>
2026-09-02 23:41:53 +0600

    top massage done

--- lib/features/studentDashord/view/student_dashbord_screen.dart
+++ lib/features/studentDashord/view/student_dashbord_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../core/core.dart';
 import '../../../core/utils/app_images.dart';
 import '../../../core/widgets/custom_top_design.dart';
+import '../widgets/top_massage.dart';
 
 class StudentDashbordScreen extends StatelessWidget {
   const StudentDashbordScreen({super.key});
@@ -11,7 +14,21 @@ class StudentDashbordScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          children: [CustomTopDesign(iconPath: ImagePath.happyImage),
+          children: [
+            CustomTopDesign(iconPath: ImagePath.happyImage),
+
+            AppDimensions.spaceXL.h.verticalSpace,
+
+            Column(
+              children: const [
+                TopMassageCard(
+                  title: 'Welcome Message',
+                  subtitle: 'The standard Lorem Ipsum passage',
+                  message:
+                      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                ),
+              ],
+            ),
           ],
         ),
       ),
---
+++ lib/features/studentDashord/widgets/top_massage.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../core/core.dart';
+import '../../../core/widgets/custom_loader.dart';
+
+class TopMassageCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String message;
+
+  const TopMassageCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.message,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(24),
+      decoration: BoxDecoration(
+        color: const Color(0xFF1546C9), // Deep blue color matching the image
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children: [
+              CustomText(
+                text: title,
+                fontWeight: FontWeight.w500,
+                fontSize: AppDimensions.fontXL.sp,
+                color: ShipXColors.background,
+              ),
+              AppDimensions.spaceL.w.horizontalSpace,
+              Icon(
+                Icons.arrow_forward,
+                color: Colors.white,
+                size: AppDimensions.iconXL.r,
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          CustomText(
+            text: subtitle,
+            color: ShipXColors.background,
+            fontSize: AppDimensions.fontM,
+          ),
+          const SizedBox(height: 6),
+          CustomText(
+            text: message,
+            color: ShipXColors.background,
+            fontSize: 14,
+          ),
+        ],
+      ),
+    );
+  }
+}
